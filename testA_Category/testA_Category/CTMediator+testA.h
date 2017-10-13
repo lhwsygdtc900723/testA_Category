5e1bcb384de6b2ba6c8d2ad73abaf92b6ffe51bd
lihui <195814193@qq.com>
2017-10-13 16:16:01 +0800

    1

---
+++ testA_Category/testA_Category/CTMediator+testA.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+testA.h
+//  testA_Category
+//
+//  Created by lihui on 2017/10/13.
+//  Copyright © 2017年 lihui. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator (testA)
+- (UIViewController *)testA_aViewController;
+@end
---
+++ upload.sh
@@ -0,0 +1,1 @@
+pod repo push testPrivatePod testA_Category.podspec --verbose --allow-warnings
