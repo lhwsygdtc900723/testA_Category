f7a5733a7c66e1b6f45eb07086edc53a09cef854
lihui <195814193@qq.com>
2017-10-13 11:16:39 +0800

    Initial Commit

---
+++ testA_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testA_Category
+//
+//  Created by lihui on 2017/10/13.
+//  Copyright © 2017年 lihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
